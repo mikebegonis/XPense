1ee3456c17706ed600c06d41c86652082c5e5822
mikebegonis <begonis13@gmail.com>
2015-04-02 10:56:11 -0400

    Added XPense background Service Module

---
+++ XPense/app/src/main/java/com/bekoal/xpense/SummaryActivity.java
@@ -0,0 +1,66 @@
+package com.bekoal.xpense;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+
+public class SummaryActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create buttons
+        final Button summaryButton = (Button) findViewById(R.id.summary_button);
+        final Button addButton = (Button) findViewById(R.id.add_button);
+        final Button travelModeButton = (Button) findViewById(R.id.travel_mode_button);
+
+        summaryButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Dummy button, does nothing
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SummaryActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        travelModeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(SummaryActivity.this, TravelModeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}

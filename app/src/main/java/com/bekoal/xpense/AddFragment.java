7ae3cbbc807fb15927b66f79bc49a456f082131a
mikebegonis <begonis13@gmail.com>
2015-04-09 15:03:34 -0400

    Implemented Add Expense Fragment

--- app/src/main/java/com/bekoal/xpense/AddFragment.java
+++ app/src/main/java/com/bekoal/xpense/AddFragment.java
@@ -10,13 +10,62 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class AddFragment extends Fragment {
+
+    ImageButton btnConfirm = null;
+    ImageButton btnCancel = null;
+    EditText txtDateTimeExpense = null;
+    EditText txtAmountExpense = null;
+    Spinner spinnerExpenseType = null;
+    EditText txtDescription = null;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.add_fragment, container, false);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        btnConfirm = (ImageButton)getView().findViewById(R.id.btn_add_expense_confirm);
+        btnCancel = (ImageButton)getView().findViewById(R.id.btn_add_expense_cancel);
+        txtDateTimeExpense = (EditText)getView().findViewById(R.id.txtDateTime_Expense);
+        txtAmountExpense = (EditText)getView().findViewById(R.id.txtAmount_Expense);
+        txtDescription = (EditText)getView().findViewById(R.id.txtDescription_Expense);
+        spinnerExpenseType = (Spinner)getView().findViewById(R.id.spinnerExpenseType);
+
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strQuery = "INSERT INTO Expenses VALUES(";
+                strQuery += String.format("'%s', NULL, %f, '%s', NULL, 0, 0, 0, '%s');",
+                        txtDateTimeExpense.getText().toString(), txtAmountExpense.getText().toString(),
+                        txtDescription.getText().toString(),
+                        spinnerExpenseType.getSelectedItem().toString());
+
+                
+
+
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 
     @Override
